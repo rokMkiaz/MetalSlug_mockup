29d3bba82eced372e6cf51ee74424f55a630e01c
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-11-07 11:58:49 +0900

    Update PS.hlsl

--- Engine/Shader/HLSL/PS.hlsl
+++ Engine/Shader/HLSL/PS.hlsl
@@ -1,12 +1,12 @@
 #include"IL.hlsli"
 
-Texture2D Texture : register(T0); //레지스터는 순서를 뜻함
+Texture2D Texture : register(T0); 
 
 float4 PS(const Pixel Input) : SV_TARGET
 {
 	const float4 Output =
 	{
-		Texture.Load(int3(Input.TexCoord, 0)) //몇번 min맵에서 가져올지
+		Texture.Load(int3(Input.TexCoord, 0)) 
 	};
 	
 	if (Output.a!=1)
