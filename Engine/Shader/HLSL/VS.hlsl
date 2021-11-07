7a86311d4f4aab1534d649cdd100aabd670fcf44
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-11-07 11:59:15 +0900

    Update VS.hlsl

--- Engine/Shader/HLSL/VS.hlsl
+++ Engine/Shader/HLSL/VS.hlsl
@@ -1,6 +1,6 @@
 #include"IL.hlsli"
 
-cbuffer Transform: register(b0){ matrix World      ;} // romajor 행 우선 행렬 지정
+cbuffer Transform: register(b0){ matrix World      ;} 
 cbuffer Transform: register(b1){ matrix View       ;}
 cbuffer Transform: register(b2){ matrix Projection ;}
 
