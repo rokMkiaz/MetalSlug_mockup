07b85533881a78882e5c6d115282aa542d5fc41e
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2021-11-08 14:08:09 +0900

    Update Engine.cpp

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -31,23 +31,20 @@ namespace Engine
         UINT const uMessage,
         WPARAM const wParameter,
         LPARAM const lParameter 
-        /*W,L PARAM메세지별 세부 정보 정확한 내용은 내부적으로 나뉘어져있음*/
+       
     )
 
     {
-        //윈도우 프로시저 특정 윈도우 클래스의 인스턴스로부터 발생한 메세지를 처리하는 함수.
+       
 
 
         switch (uMessage)
         {
 
-        case WM_CREATE:    //생성직후 생성자와 같음, lParmeter로 CRATESTRUCT Window의 인자값이 포인터로 입력
+        case WM_CREATE:    
         {
    
-            //과제: 윈도우즈좌표계의 한점을 월드좌표계로 변환하려면 어떤변환이 필요한가?
-
-
-
+        
             Rendering::Procedure(hWindow, uMessage, wParameter, lParameter);
 
           
@@ -72,7 +69,7 @@ namespace Engine
 
             return 0;
         }
-        case WM_DESTROY: //창이 파괴 될때  소멸자와 동일
+        case WM_DESTROY: 
         {
 
             Portfolio->End();
@@ -80,7 +77,7 @@ namespace Engine
   
             Rendering::Procedure(hWindow, uMessage, wParameter, lParameter);
 
-            PostQuitMessage(0); //프로그램 완전 종료를 위해 필요.
+            PostQuitMessage(0); 
 
             return 0;
         }
@@ -102,9 +99,9 @@ namespace Engine
             Rendering::Procedure(hWindow, uMessage, wParameter, lParameter);
             return 0;
         }
-        default: //기본 상태
+        default: 
         {
-            return DefWindowProc(hWindow, uMessage, wParameter, lParameter);    // Close동작을 할 경우 자동으로 Destroy명령을 내린다.
+            return DefWindowProc(hWindow, uMessage, wParameter, lParameter);    
         }
         break;
         }
